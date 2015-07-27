e2210a32e4c52080aab2cff86f737f54fcf66165
danzaharia <zaharia.danny@gmail.com>
2015-07-26 19:24:40 -0500

    Use socket io to share colors

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-var express = require('express');
 var path = require('path');
-
-//
-var handlebars = require('express-handlebars');
-//
-
+var express = require('express');
 var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var handlebars = require('express-handlebars');
 
 var localport = '3333';
 var localhost = 'http://localhost';
+
+var currMayor = 0;
 
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -16,7 +16,28 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.engine('hbs', handlebars({ extname: 'hbs', defaultLayout: 'layout.hbs' }));
 app.set('view engine', 'hbs');
 
+io.on('connection', function(socket) {
+	console.log('received connection');
 
+	socket.on('change mayor', function(data) {
+		currMayor = data.clientId;
+		console.log('The new mayor is ' + currMayor);
+	});
+
+	socket.on('change mode', function(data) {
+		if (data.clientId = currMayor) {
+			socket.emit('change mode', data);
+			console.log('Mode changed to ' + data.mode + ' by ' + data.clientId);
+		}
+	});
+
+	socket.on('change color', function(data) {
+		if (data.clientId = currMayor) {
+			socket.emit('change color', data);
+			console.log('color changed by ' + data.clientId);
+		}
+	});
+});
 
 app.host = app.set('host', process.env.HOST || localhost);
 app.port = app.set('port', process.env.PORT || localport);
@@ -27,7 +48,7 @@ app.get('/', function(req, res) {
 	res.render('index');
 });
 
-var server = app.listen(app.get('port'), function() {
+server.listen(app.get('port'), function() {
   app.address = app.get('host') + ':' + server.address().port;
   console.log('Listening at ' + app.address);
 });
